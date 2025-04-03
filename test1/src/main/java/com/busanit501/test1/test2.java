025fd1ec135a612d7ea3c39b956a6d9a394247f5
kimdh32022 <kimdh32022@naver.com>
2025-04-03 11:37:51 +0900

    진짜 완료?

--- test1/src/main/java/com/busanit501/test1/test2.java
+++ test1/src/main/java/com/busanit501/test1/test2.java
@@ -1,4 +1,26 @@
 package com.busanit501.test1;
 
-public class test2 {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name ="mytest1", urlPatterns = "/my")
+public class test2 extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>test2</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>test2</h1>");
+        out.println("<h2>이제 됐다 시발!</h2></body>");
+        out.println("</html>");
+        out.close();
+    }
 }
