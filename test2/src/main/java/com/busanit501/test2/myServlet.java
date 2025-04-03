709285507fb5fde8cdb9159559291a7d21b650eb
kimdh32022 <kimdh32022@naver.com>
2025-04-03 11:49:04 +0900

    다시 한번더

--- test2/src/main/java/com/busanit501/test2/myServlet.java
+++ test2/src/main/java/com/busanit501/test2/myServlet.java
@@ -1,4 +1,26 @@
 package com.busanit501.test2;
 
-public class myServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "/servlet", urlPatterns = "/my")
+public class myServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>my title</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>my title</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
 }
