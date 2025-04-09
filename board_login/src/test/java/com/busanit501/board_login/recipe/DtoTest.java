8e046c0ede93ba01e713e50d7e27bf419c946bb0
kimdh32022 <kimdh32022@naver.com>
2025-04-09 15:12:55 +0900

    HikariCP를 이용해서 DB접근 시 자원 이용을 효율적으로 하기

--- board_login/src/test/java/com/busanit501/board_login/recipe/DtoTest.java
+++ board_login/src/test/java/com/busanit501/board_login/recipe/DtoTest.java
@@ -1,12 +1,30 @@
 package com.busanit501.board_login.recipe;
 
-import com.busanit501.board_login.recipe.dto.RecipeDTO;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DtoTest {
     @Test
-    public void test() {
-        RecipeDTO recipeDTO = new RecipeDTO();
+    public void test() throws SQLException {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("org.mariadb.jdbc.Driver");
+        hikariConfig.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        hikariConfig.setUsername("webuser");
+        hikariConfig.setPassword("webuser");
+
+        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
+        Connection connection = hikariDataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
 
     }
+
 }
