3ae716dd3f99ad44df7ac0569fecac65dde6ddf7
kimdh32022 <kimdh32022@naver.com>
2025-04-09 10:34:45 +0900

    마리아DB 연결 확인중

--- board_login/src/test/java/com/busanit501/board_login/ConnectTest.java
+++ board_login/src/test/java/com/busanit501/board_login/ConnectTest.java
@@ -3,6 +3,10 @@ package com.busanit501.board_login;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectTest {
     @Test
     public void test() {
@@ -10,5 +14,19 @@ public class ConnectTest {
         int v2 = 100;
 
         Assertions.assertEquals(v1, v2);
+    }
+
+
+    // DB연결 테스트 하기
+    @Test
+    public void test2() throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        Connection con = DriverManager.getConnection
+                ("jdbc:mariadb://localhost:3306/webdb", "webuser", "webuser");
+
+        Assertions.assertNotNull(con);
+
+        con.close();
     }
 }
