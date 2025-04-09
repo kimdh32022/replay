af05ab7724fa2d56996adc77d15052ed838d8fc7
kimdh32022 <kimdh32022@naver.com>
2025-04-09 10:29:17 +0900

    마리아DB를 연결하고 잘 연결하였는지 확인하기 위해 단위 테스트를 실행하는것 2024.11.22일 2교시 수업중

---
+++ board_login/src/test/java/com/busanit501/board_login/ConnectTest.java
@@ -0,0 +1,14 @@
+package com.busanit501.board_login;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConnectTest {
+    @Test
+    public void test() {
+        int v1 = 100;
+        int v2 = 100;
+
+        Assertions.assertEquals(v1, v2);
+    }
+}
